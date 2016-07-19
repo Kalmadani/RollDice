50fda5da6672867bc1f41a86d58c7c44bebe0980
Kal <khalid.almadani@agilethought.com>
2016-07-19 16:29:04 -0400

    added rice program

---
+++ rollDice while loop/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rollDice_while_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            start:
+            Random rollDice = new Random();
+
+            int numberOfAttempts = 0;
+            int attempt = 0;
+
+            while (attempt != 6)
+            {
+                attempt = rollDice.Next(1, 7);
+                Console.WriteLine("Jason rolled a " + attempt);
+                numberOfAttempts++;
+            }
+
+            Console.WriteLine("It took Jason  " + numberOfAttempts + " trys to roll a six");
+            Console.ReadKey();
+            Console.WriteLine();
+            goto start;
+
+
+        }
+    }
+}
